5351d312b84521a7862603e36a7bdb752c28338d
SC19019 <59403111+SC19019@users.noreply.github.com>
2020-01-02 23:38:43 +0800

    Update StatCompR.R

--- R/StatCompR.R
+++ R/StatCompR.R
@@ -1,11 +1,11 @@
  fr=function(x){
-#    z=rnorm(100)
-#    d=sort(abs(z-x))
-#    y=numeric(100)
-#    for(k in 1:100){
-#      y[k]=k/2/100/d[k]
-#    }
-#    dis=abs(y-dnorm(x))
-#    re=which.min(dis)
-#    return(re)
-#  }
+    z=rnorm(100)
+    d=sort(abs(z-x))
+    y=numeric(100)
+    for(k in 1:100){
+      y[k]=k/2/100/d[k]
+    }
+    dis=abs(y-dnorm(x))
+    re=which.min(dis)
+    return(re)
+  }
