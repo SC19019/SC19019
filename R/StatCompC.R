920d1eb540478c60c6210ab6f82c8f4eb26ea703
SC19019 <59403111+SC19019@users.noreply.github.com>
2020-01-02 21:01:35 +0800

    Update StatCompC.R

--- R/StatCompC.R
+++ R/StatCompC.R
@@ -1,6 +1,11 @@
 library(Rcpp) # Attach R package "Rcpp"
-# Define function "fc"
-cppFunction('int fc(int x, int y, int z) {
-int sum = x + y + z;
-return sum;
+#find the index of min
+cppFunction('List findmin(NumericVector x,int l){
+double min=x[0];
+int k;
+for(int i=1;i<l;++i){
+if(min>x[i]) {min=x[i];k=i+1;}
+}
+List s=List::create(min,k);
+return s;
 }')
